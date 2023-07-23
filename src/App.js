65b1d285b6609c786708dee11f556b1ed1f7490d
Destello999 <pokego9175@gmail.com>
2023-07-23 15:37:56 +0530

    working of header and footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-// import { 
-//   BrowserRouter,
-//   Routes,
-//   Route, 
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import Footer from "./component/Footer";
 import HeroSection from "./component/HeroSection";
 import { Navbar } from "./component/Navbar";
+import Aboutus from "./page/Aboutus";
+import TempNavbar from "./component/TempNavbar";
+import TempFooter from "./component/TempFooter";
 
 function App() {
   return (
-    <div >
-      <Navbar/>
-      <HeroSection/>
-      <Footer/>
-    </div>
+    <>
+      <TempNavbar></TempNavbar>
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/about" element={<Aboutus/>} />        
+        <Route path="/login" element={<Aboutus/>} />        
+        <Route path="/signup" element={<Aboutus/>} />        
+        <Route path="/Contact" element={<Aboutus/>} />        
+      </Routes>
+      {/* <Footer/> */}
+      <TempFooter></TempFooter>
+
+     
+    </>
+
   );
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,31 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export const Navbar = () => {
     return (
         <div className='flex justify-between  dark:bg-neutral-600  '>
+            <Link to="/">
+                <h1 className='p-4 text-2xl text-stone-50'>
+                    Dream Deal
+                </h1>
+            </Link>
+             <ul className='flex p-4 space-x-4 '>
+                 <Link to="/login" className='hover:text-[17px] ease-in hover:transition duration-200 text-white active:text-gray-300' >Login</Link>
+                 <Link to="/signup" className='hover:text-[17px] hover:transition duration-200 ease-in text-white active:text-gray-300'>Sign up</Link>
+                 <Link to="/about" className='hover:text-[17px] hover:transition duration-200 ease-in text-white active:text-gray-300'>About Us</Link>
+                 <Link to="/Contact" className='hover:text-[17px] hover:transition duration-200 ease-in text-white active:text-gray-300'>Contact Us</Link>
+             </ul>
+     </div>
 
-            <h1 className='p-4 text-2xl text-stone-50'>
-                Dream Deal
-            </h1>
-
-            <ul className='flex p-4 space-x-4 '>
-                <a href="" className='hover:text-xl text-white'>
-                    <li>Login</li>
-                </a>
-                <a href="" className='hover:text-lg text-white'>
-
-                    <li>SignUp</li>
-                </a>
-                <a href="" className='hover:text-lg text-white'>
-
-                    <li>About Us</li>
-                </a>
-                <a href="" className='hover:text-lg text-white'>
-
-                    <li>Contact Us</li>
-                </a>
-            </ul>
-
-        </div>
     )
 }
