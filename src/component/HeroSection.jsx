1e03b4d77fabc53d49067d2d90f8079dc0cc1660
Destello99 <pokego9175@gmail.com>
2023-07-24 17:20:45 +0530

    framer added

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
+import { motion } from "framer-motion"
 
 export default function HeroSection() {
     return (
         <div className=" flex h-48 max-h-full items-center text-center justify-center">
             <div className=''>
-                <h1 className='text-[4rem] font-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-red-500'>
+                <motion.h1 className='text-[4rem] font-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-red-500'
+                    initial={{y:-200}}
+                    animate={{ y: 0 }}
+                    transition={{ delay: 0.9 }}
+                >
                     DREAM DEAL
                     <span>
                         <Typewriter
@@ -19,8 +24,8 @@ export default function HeroSection() {
                             
                         />
                     </span>
-                </h1>
-                <p>
+                </motion.h1>
+                <p >
                     start your Dream Deal here
                 </p>
             </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { motion } from "framer-motion"
 export default function TempNavbar() {
     return (
         <>
@@ -8,13 +8,17 @@ export default function TempNavbar() {
             <nav className="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
                 <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                     {/* Logo */}
-                    <div className="text-indigo-500 md:order-1">
+                    <motion.div className="text-indigo-500 md:order-1"
+                        initial={{ x: -500 }}
+                        animate={{ x: 0 }}
+                        transition={{ delay: 0.5 }}
+                    >
                         {/* Heroicon - Chip Outline */}
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-10 w-10"
                             fill="none"
-                            viewBox="0 0 34 24"
+                            viewBox=" 0 34 24"
                             stroke="currentColor"
                         >
                             <path
@@ -24,9 +28,11 @@ export default function TempNavbar() {
                                 d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
                             />
                         </svg>
-                    </div>
+                    </motion.div>
                     <div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
-                        <ul className="flex font-semibold justify-between">
+                        <motion.ul className="flex font-semibold justify-between"
+                            whileHover={{ scale: 1.1}}
+                        >
                             <Link to="/" className='md:px-4 md:py-2 focus:text-indigo-500'>
                                 Home
                             </Link>
@@ -42,12 +48,16 @@ export default function TempNavbar() {
                             <Link to="/signup" className=" focus:text-indigo-500 md:px-4 md:py-2 hover:text-indigo-400">
                                 Contact Us
                             </Link>
-                        </ul>
+                        </motion.ul>
                     </div>
                     <div className="order-2 md:order-3">
 
                         <Link to="/login">
-                            <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+                            <motion.button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2"
+                                initial={{ x: 325 }}
+                                animate={{ x: 50 }}
+                                transition={{ delay: 0.5 }}
+                            >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-5 w-5"
@@ -60,7 +70,7 @@ export default function TempNavbar() {
                                     />
                                 </svg>
                                 <span>Login</span>
-                            </button>
+                            </motion.button>
                         </Link>
                     </div>
                 </div>
